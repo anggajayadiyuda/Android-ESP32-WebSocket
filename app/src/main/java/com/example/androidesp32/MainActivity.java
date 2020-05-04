e687f52b1f7d63c3718611e28d8a8633b2e22da0
anggajayadiyuda <anggajayadiyuda@gmail.com>
2020-05-04 15:36:58 +0700

    Update ESP32

--- app/src/main/java/com/example/androidesp32/MainActivity.java
+++ app/src/main/java/com/example/androidesp32/MainActivity.java
@@ -15,44 +15,36 @@ import okhttp3.WebSocketListener;
 import okio.ByteString;
 
 public class MainActivity extends AppCompatActivity {
-
     private Button start;
     private TextView output;
     private OkHttpClient client;
-
-    private final class EchoWebSocket extends WebSocketListener implements com.example.androidesp32.EchoWebSocket {
+    private final class EchoWebSocketListener extends WebSocketListener {
         private static final int NORMAL_CLOSURE_STATUS = 1000;
         @Override
-        public void onOpen(WebSocket webSocket, Response response){
-            webSocket.send("Helo!!!");
-            webSocket.send("Yo Bro");
-            webSocket.send(ByteString.decodeHex("COBA-COBA"));
-            webSocket.close(NORMAL_CLOSURE_STATUS, "BYE-BYE");
+        public void onOpen(WebSocket webSocket, Response response) {
+            webSocket.send("Hello, it's Bro");
+            webSocket.send("What's up ?");
+            webSocket.send(ByteString.decodeHex("deadbeef"));
+//            webSocket.close(NORMAL_CLOSURE_STATUS, "Goodbye !");
         }
         @Override
-        public void onMessage(WebSocket webSocket, String text){
-            output("Terima = " +text);
+        public void onMessage(WebSocket webSocket, String text) {
+            output("Receiving : " + text);
         }
-
         @Override
-        public void onByte(WebSocket webSocket, ByteString bytes){
-            output("Receiving bytes = " + bytes.hex());
+        public void onMessage(WebSocket webSocket, ByteString bytes) {
+            output("Receiving bytes : " + bytes.hex());
         }
-
         @Override
-        public void onClosing(WebSocket webSocket, int code, String reason){
+        public void onClosing(WebSocket webSocket, int code, String reason) {
             webSocket.close(NORMAL_CLOSURE_STATUS, null);
-            output("Closing = " + code + " /" +reason);
+            output("Closing : " + code + " / " + reason);
         }
-
         @Override
-        public void onFailure(WebSocket webSocket, Throwable t, Response response){
-            output("Error = " + t.getMessage());
+        public void onFailure(WebSocket webSocket, Throwable t, Response response) {
+            output("Error : " + t.getMessage());
         }
     }
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -60,27 +52,24 @@ public class MainActivity extends AppCompatActivity {
         start = (Button) findViewById(R.id.start);
         output = (TextView) findViewById(R.id.output);
         client = new OkHttpClient();
-
         start.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
                 start();
             }
         });
-
     }
-    private void start(){
-        Request request = new Request.Builder().url("ws://echo.websocket.org").build();
-        EchoWebSocket Listener = new EchoWebSocket();
-        WebSocket ws = client.newWebSocket(request, Listener);
-
+    private void start() {
+        Request request = new Request.Builder().url("ws://10.39.26.107:80").build();
+        EchoWebSocketListener listener = new EchoWebSocketListener();
+        WebSocket ws = client.newWebSocket(request, listener);
         client.dispatcher().executorService().shutdown();
     }
-    private void output(final String txt){
+    private void output(final String txt) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                output.setText(output.getText().toString()+"\n\n"+txt);
+                output.setText(output.getText().toString() + "\n\n" + txt);
             }
         });
     }

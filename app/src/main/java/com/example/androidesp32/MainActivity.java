89e2c48132f4716601d365fe9bb2f190eba94582
anggajayadiyuda <anggajayadiyuda@gmail.com>
2020-05-06 12:36:35 +0700

    JSON Parsing
    Tinggal ambil data JSONnya ke addEntry, tapi baru keambil data yang pertama saja.

--- app/src/main/java/com/example/androidesp32/MainActivity.java
+++ app/src/main/java/com/example/androidesp32/MainActivity.java
@@ -44,8 +44,12 @@ import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements OnChartValueSelectedListener {
     LineChart mChart;
+    LineChart mChart1;
+    LineChart mChart2;
     String pesanutuh="";
-    double nilai = 0;
+    double nilai, nilai1, nilai2 = 0;
+    public String pesan = "";
+    public String pesanascii = "";
     private Button start;
     private EditText editText;
     private TextView output;
@@ -108,48 +112,46 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
             }
         });
 
+    chartmChart();
+    chartmChart1();
+    chartmChart2();
+    }
+
+    public void chartmChart(){
         mChart = findViewById(R.id.line_chart);
         mChart.setOnChartValueSelectedListener(this);
-
         // enable description text
         mChart.getDescription().setEnabled(false);
-
         // enable touch gestures
         mChart.setTouchEnabled(true);
-
         // enable scaling and dragging
         mChart.setDragEnabled(true);
         mChart.setScaleEnabled(true);
         mChart.setDrawGridBackground(false);
-
         // if disabled, scaling can be done on x- and y-axis separately
         mChart.setPinchZoom(true);
-
         // set an alternative background color
-        mChart.setBackgroundColor(Color.LTGRAY);
+//        mChart.setBackgroundColor(Color.BLACK);
+        mChart.setDrawBorders(true);
+        mChart.setBorderColor(Color.WHITE);
+        mChart.setBorderWidth(1);
 
         LineData data = new LineData();
         data.setValueTextColor(Color.WHITE);
         // add empty data
         mChart.setData(data);
-
-//        feedMultiple();
         // get the legend (only possible after setting data)
         Legend l = mChart.getLegend();
-
         // modify the legend ...
         l.setForm(Legend.LegendForm.LINE);
-
 //        l.setTypeface(tfLight);
         l.setTextColor(Color.WHITE);
-
         XAxis xl = mChart.getXAxis();
 //        xl.setTypeface(tfLight);
         xl.setTextColor(Color.WHITE);
         xl.setDrawGridLines(false);
         xl.setAvoidFirstLastClipping(true);
         xl.setEnabled(true);
-
         YAxis leftAxis = mChart.getAxisLeft();
 //        leftAxis.setTypeface(tfLight);
         leftAxis.setTextColor(Color.WHITE);
@@ -158,52 +160,83 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
         leftAxis.setDrawGridLines(true);
         YAxis rightAxis = mChart.getAxisRight();
         rightAxis.setEnabled(false);
-
     }
-
-    private void addEntry(float value) {
-
-        LineData data = mChart.getData();
-
-        if (data != null) {
-
-            ILineDataSet set = data.getDataSetByIndex(0);
-            // set.addEntry(...); // can be called as well
-
-            if (set == null) {
-                set = createSet();
-                data.addDataSet(set);
-            }
-
-            data.addEntry(new Entry(set.getEntryCount(), (float) (value)), 0);
-//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 40) + 30f), 0);
-
-//            System.out.println(set.getEntryCount());
-            data.notifyDataChanged();
-
-            // let the chart know it's data has changed
-            mChart.notifyDataSetChanged();
-
-            // limit the number of visible entries
-            mChart.setVisibleXRangeMaximum(120);
-            // chart.setVisibleYRange(30, AxisDependency.LEFT);
-
-            // move to the latest entry
-            mChart.moveViewToX(data.getEntryCount());
-            mChart.invalidate();
-            // this automatically refreshes the chart (calls invalidate())
-            // chart.moveViewTo(data.getXValCount()-7, 55f,
-            // AxisDependency.LEFT);
-        }
-
+    public void chartmChart1(){
+        mChart1 = findViewById(R.id.line_chart1);
+        mChart1.setOnChartValueSelectedListener(this);
+        mChart1.getDescription().setEnabled(false);
+        mChart1.setTouchEnabled(true);
+        mChart1.setDragEnabled(true);
+        mChart1.setScaleEnabled(true);
+        mChart1.setDrawGridBackground(false);
+        mChart1.setPinchZoom(true);
+//        mChart1.setBackgroundColor(Color.BLACK);
+        mChart1.setDrawBorders(true);
+        mChart1.setBorderColor(Color.WHITE);
+        mChart1.setBorderWidth(1);
+        LineData data1 = new LineData();
+        data1.setValueTextColor(Color.WHITE);
+        mChart1.setData(data1);
+        Legend l = mChart1.getLegend();
+        l.setForm(Legend.LegendForm.LINE);
+//        l.setTypeface(tfLight);
+        l.setTextColor(Color.WHITE);
+        XAxis xl = mChart1.getXAxis();
+//        xl.setTypeface(tfLight);
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+        YAxis leftAxis = mChart1.getAxisLeft();
+//        leftAxis.setTypeface(tfLight);
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        YAxis rightAxis = mChart1.getAxisRight();
+        rightAxis.setEnabled(false);
     }
+    public void chartmChart2(){
+        mChart2 = findViewById(R.id.line_chart2);
+        mChart2.setOnChartValueSelectedListener(this);
+        mChart2.getDescription().setEnabled(false);
+        mChart2.setTouchEnabled(true);
+        mChart2.setDragEnabled(true);
+        mChart2.setScaleEnabled(true);
+        mChart2.setDrawGridBackground(false);
+        mChart2.setPinchZoom(true);
+//        mChart2.setBackgroundColor(Color.BLACK);
+        mChart2.setDrawBorders(true);
+        mChart2.setBorderColor(Color.WHITE);
+        mChart2.setBorderWidth(1);
+        LineData data2 = new LineData();
+        data2.setValueTextColor(Color.WHITE);
+        mChart2.setData(data2);
+        Legend l = mChart2.getLegend();
 
+        l.setForm(Legend.LegendForm.LINE);
+//        l.setTypeface(tfLight);
+        l.setTextColor(Color.WHITE);
+        XAxis xl = mChart2.getXAxis();
+//        xl.setTypeface(tfLight);
+        xl.setTextColor(Color.WHITE);
+        xl.setDrawGridLines(false);
+        xl.setAvoidFirstLastClipping(true);
+        xl.setEnabled(true);
+        YAxis leftAxis = mChart2.getAxisLeft();
+//        leftAxis.setTypeface(tfLight);
+        leftAxis.setTextColor(Color.WHITE);
+        leftAxis.setAxisMaximum(100f);
+        leftAxis.setAxisMinimum(0f);
+        leftAxis.setDrawGridLines(true);
+        YAxis rightAxis = mChart2.getAxisRight();
+        rightAxis.setEnabled(false);
+    }
     private LineDataSet createSet() {
-
-        LineDataSet set = new LineDataSet(null, "Dynamic Data");
+        LineDataSet set = new LineDataSet(null, "Tekanan 1");
         set.setAxisDependency(YAxis.AxisDependency.LEFT);
         set.setColor(ColorTemplate.getHoloBlue());
-        set.setCircleColor(Color.WHITE);
+        set.setCircleColor(ColorTemplate.getHoloBlue());
         set.setLineWidth(2f);
         set.setCircleRadius(4f);
         set.setFillAlpha(65);
@@ -215,43 +248,197 @@ public class MainActivity extends AppCompatActivity implements OnChartValueSelec
         set.setDrawCircles(false);
         return set;
     }
+    private LineDataSet createSet1() {
+        LineDataSet set1 = new LineDataSet(null, "Tekanan 2");
+        set1.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set1.setColor(Color.rgb(0, 255, 0));
+        set1.setCircleColor(ColorTemplate.getHoloBlue());
+        set1.setLineWidth(2f);
+        set1.setCircleRadius(4f);
+        set1.setFillAlpha(65);
+        set1.setFillColor(ColorTemplate.getHoloBlue());
+        set1.setHighLightColor(Color.rgb(244, 117, 117));
+        set1.setValueTextColor(Color.WHITE);
+        set1.setValueTextSize(9f);
+        set1.setDrawValues(false);
+        set1.setDrawCircles(false);
+        return set1;
+    }
+    private LineDataSet createSet2() {
+        LineDataSet set2 = new LineDataSet(null, "Tekanan 3");
+        set2.setAxisDependency(YAxis.AxisDependency.LEFT);
+        set2.setColor(Color.MAGENTA);
+        set2.setCircleColor(ColorTemplate.getHoloBlue());
+        set2.setLineWidth(2f);
+        set2.setCircleRadius(4f);
+        set2.setFillAlpha(65);
+        set2.setFillColor(ColorTemplate.getHoloBlue());
+        set2.setHighLightColor(Color.rgb(244, 117, 117));
+        set2.setValueTextColor(Color.WHITE);
+        set2.setValueTextSize(9f);
+        set2.setDrawValues(false);
+        set2.setDrawCircles(false);
+        return set2;
+    }
+    private void addEntry(float value) {
+        LineData data = mChart.getData();
+        if (data != null) {
+            ILineDataSet set = data.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+            if (set == null) {
+                set = createSet();
+                data.addDataSet(set);
+            }
+            data.addEntry(new Entry(set.getEntryCount(), (float) (value)), 0);
+//            data.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 76) + 30f), 0);
+//            System.out.println(set.getEntryCount());
+            data.notifyDataChanged();
+            // let the chart know it's data has changed
+            mChart.notifyDataSetChanged();
+            // limit the number of visible entries
+            mChart.setVisibleXRangeMaximum(30);
+            // chart.setVisibleYRange(30, AxisDependency.LEFT);
+            // move to the latest entry
+            mChart.moveViewToX(data.getEntryCount());
+            mChart.invalidate();
+            // this automatically refreshes the chart (calls invalidate())
+            // chart.moveViewTo(data.getXValCount()-7, 55f,
+            // AxisDependency.LEFT);
+        }
+    }
+    private void addEntry1(float value) {
+        LineData data1 = mChart1.getData();
+        if (data1 != null) {
+            ILineDataSet set = data1.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+            if (set == null) {
+                set = createSet1();
+                data1.addDataSet(set);
+            }
+            data1.addEntry(new Entry(set.getEntryCount(), (float) (value)), 0);
+//            data1.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 45) + 21f), 0);
+//            System.out.println(set.getEntryCount());
+            data1.notifyDataChanged();
+            // let the chart know it's data has changed
+            mChart1.notifyDataSetChanged();
+            // limit the number of visible entries
+            mChart1.setVisibleXRangeMaximum(30);
+            // chart.setVisibleYRange(30, AxisDependency.LEFT);
+            // move to the latest entry
+            mChart1.moveViewToX(data1.getEntryCount());
+            mChart1.invalidate();
+            // this automatically refreshes the chart (calls invalidate())
+            // chart.moveViewTo(data1.getXValCount()-7, 55f,
+            // AxisDependency.LEFT);
+        }
+    }
+    private void addEntry2(float value) {
+        LineData data2 = mChart2.getData();
+        if (data2 != null) {
+            ILineDataSet set = data2.getDataSetByIndex(0);
+            // set.addEntry(...); // can be called as well
+            if (set == null) {
+                set = createSet2();
+                data2.addDataSet(set);
+            }
+            data2.addEntry(new Entry(set.getEntryCount(), (float) (value)), 0);
+//            data2.addEntry(new Entry(set.getEntryCount(), (float) (Math.random() * 70) + 30f), 0);
+//            System.out.println(set.getEntryCount());
+            data2.notifyDataChanged();
+            // let the chart know it's data has changed
+            mChart2.notifyDataSetChanged();
+            // limit the number of visible entries
+            mChart2.setVisibleXRangeMaximum(30);
+            // chart.setVisibleYRange(30, AxisDependency.LEFT);
+            // move to the latest entry
+            mChart2.moveViewToX(data2.getEntryCount());
+            mChart2.invalidate();
+            // this automatically refreshes the chart (calls invalidate())
+            // chart.moveViewTo(data.getXValCount()-7, 55f,
+            // AxisDependency.LEFT);
+        }
+    }
 
-//    String pesanutuh="";
-//    private void JSON(String pesan){
-//           try {
-//               JSONObject OBJ = new JSONObject(pesanutuh);
-//               double nilai = OBJ.optDouble("tekanan1", 0.0);
-//               addEntry((float)nilai);
-//           } catch (JSONException e) {
-//               e.printStackTrace();
-//               ((EditText) MainActivity.this.findViewById(R.id.editText)).setText("\n---Pesan---\n" + e.getMessage() + "\n---Errornya---\n" + e.toString());
-//           }
-//    }
+
 
     String txt = "";
     public void start() {
-        Request request = new Request.Builder().url("ws://192.168.43.38:80/test").build();
+        Request request = new Request.Builder().url("ws://192.168.137.201:80/test").build();
         EchoWebSocketListener listener = new EchoWebSocketListener();
         WebSocket ws = client.newWebSocket(request, listener);
         client.dispatcher().executorService().shutdown();
     }
+
     public void output(String txt) {
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
                 output.setText(txt);
-                pesanutuh = txt;
+                String isi = output.getText().toString();
+//                try {
+//                    JSON(isi);
+//                } catch (Exception e) {
+//                    e.printStackTrace();
+//                }
                 try {
-                    String isi = output.getText().toString();
+//                    String isi = output.getText().toString();
                     JSONObject OBJ = new JSONObject(isi);
-                    nilai = OBJ.optDouble("tekanan1", 0.0);
-                    addEntry((float) nilai);
+                        nilai = OBJ.optDouble("tekanan1", 0.0);
+                        nilai1 = OBJ.optDouble("tekanan2", 0.0);
+                        nilai2 = OBJ.optDouble("tekanan3", 0.0);
+                        addEntry((float) nilai);
+                        addEntry1((float) nilai1);
+                        addEntry2((float) nilai2);
                 } catch (JSONException e) {
                     e.printStackTrace();
                     ((EditText) MainActivity.this.findViewById(R.id.editText)).setText("\n---Pesan---\n" + e.getMessage() + "\n---Errornya---\n" + e.toString());
                 }
             }
         });
+
     }
+//    private void JSON(String pesan){
+//        if(pesan.contains("{")) {
+//            setPesanascii(false,pesan);
+//        }else if (pesan.contains("}")) {
+//            setPesanascii(true,pesan);
+//            String pesanutuh=getPesanascii(pesanascii);
+//            pesanutuh=pesanutuh.replace("\\s","");
+//            try {
+//                JSONObject obj = new JSONObject(pesanutuh);
+//                nilai  = obj.optDouble("tekanan1",0.0);
+//                nilai1 = obj.optDouble("tekanan2",0.0);
+//                nilai2 = obj.optDouble("tekanan3",0.0);
+//                addEntry((float)nilai);
+//                addEntry1((float)nilai1);
+//                addEntry2((float)nilai2);
+//            } catch (JSONException e) {
+//                e.printStackTrace();
+//            }
+//            pesanascii="";
+//        }else {
+//            setPesanascii(true,pesan);
+//        }
+//    }
+//    private void setPesanascii(boolean tambah,String pesan){
+//        if(!tambah)
+//            pesanascii="";
+//
+//        for(int i=0;i<pesan.length();i++)
+//            pesanascii+=(int)pesan.charAt(i)+",";
+//    }
+//
+//    private String getPesanascii(String asciinya) {
+//        asciinya+="0";
+//        String[] satuan=asciinya.split(",");
+//        String jadi="";
+//        for (String satu : satuan) {
+//            if(!satu.isEmpty()) {
+//                if ((char) Integer.parseInt(satu.trim()) > 0)
+//                    jadi += "" + (char) Integer.parseInt(satu.trim());
+//            }
+//        }
+//        return jadi;
+//    }
 
 }
